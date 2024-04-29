50a5baac92cab5ae504559b3e149b419ee7d71bc
Jiwoon <kfromh0136@Naver.com>
2024-04-29 11:04:10 +0900

    [setup] project setup
    [dependencies]
    - spring webflux
    - lombok
    - spring validation

---
+++ src/main/java/wanner/pay/api/wannerpayments/WannerPaymentsApplication.java
@@ -0,0 +1,13 @@
+package wanner.pay.api.wannerpayments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WannerPaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WannerPaymentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wanner/pay/api/wannerpayments/WannerPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package wanner.pay.api.wannerpayments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WannerPaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
